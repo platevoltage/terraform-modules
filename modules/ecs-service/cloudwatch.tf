3e5c7da99b013115ebcdc7c9e4d61e75df8d4765
Michael Chavez <mchavez@space-rocket.com>
2025-11-06 00:05:31 -0800

    CKV_AWS_158

--- modules/ecs-service/cloudwatch.tf
+++ modules/ecs-service/cloudwatch.tf
@@ -1,12 +1,62 @@
 #######################
-# CloudWatch Log Group
+# CloudWatch Log Group (KMS-encrypted)
 #######################
-resource "aws_cloudwatch_log_group" "fargate_task_log_group" {
-  name              = "${var.ecs_service_config.log_group_name}"
-  retention_in_days = 30
+# KMS key for CloudWatch Logs
+resource "aws_kms_key" "cloudwatch_logs" {
+  description             = "KMS key for encrypting CloudWatch Logs for ${var.ecs_service_config.log_group_name}"
+  enable_key_rotation     = true
+  deletion_window_in_days = 30
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowRootAccountAdministration",
+      "Effect": "Allow",
+      "Principal": { "AWS": "arn:aws:iam::${var.ecs_service_config.account_id}:root" },
+      "Action": "kms:*",
+      "Resource": "*"
+    },
+    {
+      "Sid": "AllowCloudWatchLogsUseOfTheKey",
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.${var.ecs_service_config.region}.amazonaws.com" },
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "ArnLike": {
+          "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:${var.ecs_service_config.region}:${var.ecs_service_config.account_id}:log-group:${var.ecs_service_config.log_group_name}*"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
 
+resource "aws_kms_alias" "cloudwatch_logs" {
+  name          = "alias/${var.ecs_service_config.app_name}-cloudwatch-logs"
+  target_key_id = aws_kms_key.cloudwatch_logs.key_id
+}
+
+# Encrypted log group
+resource "aws_cloudwatch_log_group" "fargate_task_log_group" {
+  name              = var.ecs_service_config.log_group_name
+  retention_in_days = 30
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+
+  tags = try(var.ecs_service_config.common_tags, null)
+}
+
+# Optional: dedicated stream (not required for CKV_AWS_158)
 # resource "aws_cloudwatch_log_stream" "fargate_task_log_stream" {
-#   name           = "${var.ecs_service_config.task_name}"
+#   name           = var.ecs_service_config.task_name
 #   log_group_name = aws_cloudwatch_log_group.fargate_task_log_group.name
 # }
