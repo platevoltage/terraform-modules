cc5833dcc695f5be475d4043b1fc973cbd2d5a20
Michael Chavez <mchavez@space-rocket.com>
2026-01-26 13:32:53 -0800

    minimal amount of locals needed

--- demo/03-apps/01-app1/codepipeline-locals.tf
+++ demo/03-apps/01-app1/codepipeline-locals.tf
@@ -1,31 +1,38 @@
 locals {
-  codepipeline_config = merge(
-    local.base_config,
-    local.ecs_cluster_outputs,
-    {
-      app_name                   = local.app_name
-      app_secrets                = local.app_secrets
-      app_environments           = local.app_environments
-      codebuild_compute_type     = local.codebuild_compute_type
-      codebuild_image            = local.codebuild_image
-      ecs_service_name           = module.ecs_service.ecs_service_name
-      fargate_cpu                = local.fargate_cpu
-      fargate_ecs_execution_role = local.fargate_ecs_execution_role
-      fargate_ecs_task_role      = local.fargate_ecs_task_role
-      fargate_memory             = local.fargate_memory
-      git_branch                 = local.git_branch
-      git_repo                   = local.git_repo
-      healthcheck_endpoint       = local.healthcheck_endpoint
-      image_repo                 = local.image_repo
-      log_group_name             = local.log_group_name
-      port                       = local.port
-      region                     = local.region
-      ssm_secret_path_prefix     = local.ssm_secret_path_prefix
-      task_name                  = local.task_name
+  codepipeline_config = {
+    # required by modules/codepipeline/variables.tf
+    account_id = local.base_config.account_id
+    project    = local.base_config.project
+    env        = local.base_config.env
+    region     = local.region
 
-      deploy_provider = var.deployment_strategy == "blue_green" ? "CodeDeployToECS" : "ECS"
-      codedeploy_app  = var.deployment_strategy == "blue_green" ? "${local.task_name}-cd-app" : null
-      codedeploy_dg   = var.deployment_strategy == "blue_green" ? "${local.task_name}-cd-dg"  : null
-    }
-  )
+    app_name         = local.app_name
+    task_name        = local.task_name
+    log_group_name   = local.log_group_name
+    port             = local.port
+    image_repo       = local.image_repo
+    healthcheck_endpoint = local.healthcheck_endpoint
+
+    git_repo   = local.git_repo
+    git_branch = local.git_branch
+
+    ecs_cluster_name = local.ecs_cluster_outputs.ecs_cluster_name
+    ecs_service_name = module.ecs_service.ecs_service_name
+
+    fargate_cpu                = local.fargate_cpu
+    fargate_memory             = local.fargate_memory
+    fargate_ecs_execution_role = local.fargate_ecs_execution_role
+    fargate_ecs_task_role      = local.fargate_ecs_task_role
+
+    ssm_secret_path_prefix = local.ssm_secret_path_prefix
+    app_secrets            = local.app_secrets
+    app_environments       = local.app_environments
+
+    codebuild_compute_type = local.codebuild_compute_type
+    codebuild_image        = local.codebuild_image
+
+    deploy_provider = var.deployment_strategy == "blue_green" ? "CodeDeployToECS" : "ECS"
+    codedeploy_app  = var.deployment_strategy == "blue_green" ? "${local.task_name}-cd-app" : null
+    codedeploy_dg   = var.deployment_strategy == "blue_green" ? "${local.task_name}-cd-dg"  : null
+  }
 }
--- demo/03-apps/01-app1/ecs-service-locals.tf
+++ demo/03-apps/01-app1/ecs-service-locals.tf
@@ -3,67 +3,66 @@ locals {
 }
 
 locals {
-  ecs_service_config = merge(
-    local.base_config,
-    {
-      app_count = 1
-      app_environments = [
-        { name = "MODE_TYPE", value = "app" },
-        { name = "AWS_DEFAULT_REGION", value = local.region }
-      ]
+  ecs_service_config = {
+    account_id = local.base_config.account_id
+    project    = local.base_config.project
+    env        = local.base_config.env
+    region     = local.region
 
-      app_image                  = "${local.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.image_repo}:${var.image_tag}"
-      app_name                   = local.app_name
-      app_names                  = local.app_names
-      app_port                   = var.port
-      app_secrets                = local.app_secrets
-      assign_public_ip           = false
-      ecs_cluster_id             = local.ecs_cluster_outputs.ecs_cluster_id
-      ecs_cluster_name           = local.ecs_cluster_outputs.ecs_cluster_name
-      ecs_execution_role         = local.ecs_cluster_outputs.ecs_execution_role_arn
-      fargate_cpu                = var.fargate_cpu
-      fargate_memory             = var.fargate_memory
-      fargate_subnets            = local.fargate_subnets
-      healthcheck_endpoint       = var.healthcheck_endpoint
-      healthcheck_interval       = 10
-      healthcheck_retries        = 5
-      healthcheck_start_period   = 60
-      healthcheck_timeout        = 10
-      listener_443_arn           = local.listener_443_arn
-      log_group_name             = local.log_group_name
-      path_prefix                = local.path_prefix
-      path_prefix_map            = local.base_outputs.path_prefix_map
-      region                     = local.region
-      runtime_platform           = "ARM64"
-      ssm_secret_path_prefix     = local.path_prefix
-      ssm_secret_path_prefix_map = local.base_outputs.ssm_secret_path_prefix_map
-      task_name                  = local.task_name
-      name_prefix                = local.base_outputs.name_prefix
-      common_tags                = local.base_config.common_tags
-      vpc_id                     = local.base_outputs.vpc_id
+    app_count = 1
+    app_name  = local.app_name
+    app_names = local.app_names
 
-      # rolling input
-      tg_arn = module.target_group.tg_arn
+    app_image = "${local.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.image_repo}:${var.image_tag}"
+    app_port  = var.port
 
-      # strategy inputs
-      deployment_strategy = local.deployment_strategy
+    app_environments = [
+      { name = "MODE_TYPE", value = "app" },
+      { name = "AWS_DEFAULT_REGION", value = local.region },
+    ]
+    app_secrets = local.app_secrets
 
-      # blue green extras
-      blue_tg_arn       = try(module.target_group.tg_arn, "")
-      green_tg_arn      = try(module.target_group_green[0].tg_arn, "")
+    assign_public_ip = false
 
-      # add names so CodeDeploy can rely on them even if an ARN is empty
-      blue_tg_name      = try(module.target_group.tg_name, "")
-      green_tg_name     = try(module.target_group_green[0].tg_name, "")
+    ecs_cluster_id     = local.ecs_cluster_outputs.ecs_cluster_id
+    ecs_cluster_name   = local.ecs_cluster_outputs.ecs_cluster_name
+    ecs_execution_role = local.ecs_cluster_outputs.ecs_execution_role_arn
 
-      prod_listener_arn = local.listener_443_arn
-      # Use a distinct listener for test traffic when blue_green
-      test_listener_arn = var.deployment_strategy == "blue_green" ? aws_lb_listener.test_8080.arn : null
+    fargate_cpu     = var.fargate_cpu
+    fargate_memory  = var.fargate_memory
+    fargate_subnets = local.fargate_subnets
 
-      deploy_provider            = local.deployment_strategy == "blue_green" ? "CodeDeployToECS" : "ECS"
-      codedeploy_app             = local.deployment_strategy == "blue_green" ? "${local.task_name}-cd-app" : null
-      codedeploy_dg              = local.deployment_strategy == "blue_green" ? "${local.task_name}-cd-dg"  : null
-      alb_sg_id                  = local.base_outputs.alb_sg_id
-    }
-  )
+    healthcheck_endpoint     = var.healthcheck_endpoint
+    healthcheck_interval     = 10
+    healthcheck_retries      = 5
+    healthcheck_start_period = 60
+    healthcheck_timeout      = 10
+
+    log_group_name = local.log_group_name
+
+    path_prefix_map            = local.base_outputs.path_prefix_map
+    ssm_secret_path_prefix_map = local.base_outputs.ssm_secret_path_prefix_map
+
+    task_name   = local.task_name
+    name_prefix = local.base_outputs.name_prefix
+    common_tags = local.base_config.common_tags
+    vpc_id      = local.base_outputs.vpc_id
+
+    tg_arn = module.target_group.tg_arn
+
+    deployment_strategy = local.deployment_strategy
+
+    blue_tg_arn  = try(module.target_group.tg_arn, "")
+    blue_tg_name = try(module.target_group.tg_name, "")
+
+    green_tg_arn  = try(module.target_group_green[0].tg_arn, "")
+    green_tg_name = try(module.target_group_green[0].tg_name, "")
+
+    prod_listener_arn = local.listener_443_arn
+    test_listener_arn = local.deployment_strategy == "blue_green" ? aws_lb_listener.test_8080.arn : null
+
+    alb_sg_id = local.base_outputs.alb_sg_id
+
+    runtime_platform = "ARM64"
+  }
 }
