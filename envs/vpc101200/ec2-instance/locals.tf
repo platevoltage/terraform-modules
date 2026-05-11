b0b9bd83d4fe3e9f11b54d70886f3089a2c77871
Michael Chavez <mchavez@space-rocket.com>
2026-05-10 20:37:35 -0700

    fix: enable map_public_ip_on_launch on public subnets

--- envs/vpc101200/ec2-instance/locals.tf
+++ envs/vpc101200/ec2-instance/locals.tf
@@ -10,32 +10,5 @@ locals {
     subnet_id         = local.vpc_outputs.public_subnet_ids[0]
     instance_type     = "t3.micro"
     eip_allocation_id = local.eip_outputs.allocation_id
-
-    user_data = <<-EOF
-      #!/bin/bash
-      set -e
-      dnf install -y strongswan
-      systemctl enable strongswan-starter
-      systemctl start strongswan-starter
-      echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-      sysctl -p
-    EOF
-
-    ingress_rules = [
-      {
-        description = "IKE"
-        from_port   = 500
-        to_port     = 500
-        protocol    = "udp"
-        cidr_blocks = ["0.0.0.0/0"]
-      },
-      {
-        description = "NAT-T"
-        from_port   = 4500
-        to_port     = 4500
-        protocol    = "udp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-    ]
   }
 }
--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -10,10 +10,11 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_subnet" "public" {
-  count             = var.vpc_config.create_public_subnets ? var.vpc_config.az_count : 0
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = local.public_subnet_cidrs[count.index]
-  availability_zone = local.azs[count.index]
+  count                   = var.vpc_config.create_public_subnets ? var.vpc_config.az_count : 0
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = local.public_subnet_cidrs[count.index]
+  availability_zone       = local.azs[count.index]
+  map_public_ip_on_launch = true
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-pub-${local.azs[count.index]}"
