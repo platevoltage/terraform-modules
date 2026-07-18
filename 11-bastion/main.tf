c5742ee9c2b07049f48e8d0123cf6a411c54ea99
Garrett Corbin <jgarrettcorbin@gmail.com>
2026-07-18 14:43:48 -0700

    changed bastion

--- 11-bastion/main.tf
+++ 11-bastion/main.tf
@@ -1,15 +1,7 @@
 resource "aws_security_group" "bastion" {
   name        = "${var.name_prefix}-bastion-sg"
-  description = "Allow SSH from my IP"
+  description = "Bastion access via AWS Systems Manager"
   vpc_id      = var.vpc_id
-
-  ingress {
-    description = "SSH access from my IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32"]
-  }
 
   egress {
     from_port   = 0
@@ -21,12 +13,45 @@ resource "aws_security_group" "bastion" {
   tags = var.tags
 }
 
+resource "aws_iam_role" "bastion" {
+  name = "${var.name_prefix}-bastion-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.name_prefix}-bastion-profile"
+  role = aws_iam_role.bastion.name
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_ssm" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_s3_seed" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = aws_iam_policy.bastion_s3_seed_policy.arn
+}
+
 resource "aws_instance" "bastion" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = element(var.subnet_ids, 0)
   vpc_security_group_ids = [aws_security_group.bastion.id]
-  key_name               = var.key_name
+
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   associate_public_ip_address = true
   user_data                   = var.user_data
