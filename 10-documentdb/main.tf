ad030b0a3514f51bcd4301ad22edd57c4fbd925a
Garrett Corbin <jgarrettcorbin@gmail.com>
2026-07-23 20:01:21 -0700

    skip restore

--- 10-documentdb/main.tf
+++ 10-documentdb/main.tf
@@ -67,28 +67,22 @@ resource "aws_docdb_cluster" "fresh" {
 }
 
 # Restore from snapshot if snapshot_identifier is set
-# resource "aws_docdb_cluster" "restore" {
-#   count                 = var.snapshot_identifier != null ? 1 : 0
-#   cluster_identifier    = local.cluster_identifier
-#   engine                = "docdb"
-#   snapshot_identifier   = var.snapshot_identifier
-#   db_subnet_group_name  = aws_docdb_subnet_group.this.name
-#   vpc_security_group_ids = [aws_security_group.docdb.id]
+resource "aws_docdb_cluster" "restore" {
+  count                 = var.snapshot_identifier != null ? 1 : 0
+  cluster_identifier    = local.cluster_identifier
+  engine                = "docdb"
+  snapshot_identifier   = var.snapshot_identifier
+  db_subnet_group_name  = aws_docdb_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
 
-#   tags = merge(var.tags, { Name = local.cluster_identifier })
-# }
+  tags = merge(var.tags, { Name = local.cluster_identifier })
+}
 
 # Choose active cluster values
-# locals {
-#   active_cluster_id       = try(aws_docdb_cluster.fresh[0].id, aws_docdb_cluster.restore[0].id)
-#   active_cluster_endpoint = try(aws_docdb_cluster.fresh[0].endpoint, aws_docdb_cluster.restore[0].endpoint)
-#   active_reader_endpoint  = try(aws_docdb_cluster.fresh[0].reader_endpoint, aws_docdb_cluster.restore[0].reader_endpoint)
-# }
-
 locals {
-  active_cluster_id       = aws_docdb_cluster.fresh[0].id
-  active_cluster_endpoint = aws_docdb_cluster.fresh[0].endpoint
-  active_reader_endpoint  = aws_docdb_cluster.fresh[0].reader_endpoint
+  active_cluster_id       = try(aws_docdb_cluster.fresh[0].id, aws_docdb_cluster.restore[0].id)
+  active_cluster_endpoint = try(aws_docdb_cluster.fresh[0].endpoint, aws_docdb_cluster.restore[0].endpoint)
+  active_reader_endpoint  = try(aws_docdb_cluster.fresh[0].reader_endpoint, aws_docdb_cluster.restore[0].reader_endpoint)
 }
 
 resource "aws_docdb_cluster_instance" "this" {
